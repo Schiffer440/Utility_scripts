7b16b636bd83fd589a79bf63719f7c2aaf3c02ed
pascal ion <ion.pascal@epitech.eu>
2024-11-28 17:30:31 +0100

    default message added to commit

--- push.sh
+++ push.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#!/bin/bash
+
 read -p "Which file do you want to add (* for all):" files
 git add $files
 if [ $? -ne 0 ]
 then
     exit 1
 fi
-read -p "Commit message:" message
+read -e -p "Enter commit message(leave it blank for 'auto push'): " -i "auto push" message
+echo $message
 git commit -m "$message"
 git push
