7067b883e6a104c77a906ef50c0714d1b0b0c077
Schiffer440 <arnaud.dugain@orange.fr>
2024-11-29 13:41:25 +0100

    test2

--- push.sh
+++ push.sh
@@ -3,12 +3,12 @@
 #!/bin/bash
 
 read -p "Which file do you want to add (* for all):" -i "NULL" files
-# echo $files
-# if [ $files="NULL" ]
-# then
-#     echo "No files requested, exiting..."
-#     exit 1
-# fi
+echo $files
+if [ $files="NULL" ]
+then
+    echo "No files requested, exiting..."
+    exit 1
+fi
 git add $files
 if [ $? -ne 0 ]
 then
