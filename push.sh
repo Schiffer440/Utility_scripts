3a135d662283768f399ba73717d79f6d67f15d43
Schiffer440 <arnaud.dugain@orange.fr>
2024-11-30 17:25:07 +0100

    added empty string case

--- push.sh
+++ push.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#!/bin/bash
 
-read -p "Which file do you want to add (* for all):" -i "NULL" files
-echo $files
-if [ $files="NULL" ]
+read -p "Which file do you want to add (* for all):" files
+echo "read => $files"
+if [[ -z $files ]]
 then
     echo "No files requested, exiting..."
     exit 1
+else
+    git add $files
+    if [ $? -ne 0 ]
+    then
+        exit 1
+    fi
+    read -p "Commit message:" message
+    git commit -m "$message"
+    git push
 fi
-git add $files
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
-read -p "Commit message:" message
-git commit -m "$message"
-git push
