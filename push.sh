fc82d0acf4a2246bf855da3c562a9e0bfded46c6
Schiffer440 <arnaud.dugain@orange.fr>
2024-11-30 18:01:34 +0100

    added color and style

--- push.sh
+++ push.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+#Color list#
+    DEF="\033[0m"
+    RED="\033[31;5;32m"
+    GREEN="\033[32m"
+    YELLOW="\033[33m"
+    BLUE="\033[34m"
+    MAGENTA="\033[35m"
+    CYAN="\033[36m"
+    WHITE="\033[37m"
+#Text syle#
+    BOLD="\033[1m"
+    ITALIC="\033[3m"
+    UNDERLINE="\033[4m"
 
 read -p "Which file do you want to add (* for all):" files
 echo "read => $files"
 if [[ -z $files ]]
 then
-    echo "No files requested, exiting..."
+    echo -e "${BOLD}${RED}No files requested, exiting...${DEF}"
     exit 1
 else
     git add $files
